c2e78c006e1ed0d944cde3ad7e1567fa7e36f67c
MohammadFayyad <mofhff777@gmail.com>
2026-01-30 03:02:53 +0200

    test: remove initial widget test for app startup

--- test/flutter_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ecommerce_app/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App starts without crashing', (tester) async {
-    await tester.pumpWidget(const MyApp(isLoggedIn: false));
-    expect(find.byType(MyApp), findsOneWidget);
-  });
-}
